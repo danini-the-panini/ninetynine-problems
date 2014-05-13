8258c43f016daa818e782a72fd8a9707c6d420eb
Daniel Smith <jellymann@gmail.com>
2014-05-13 17:16:22 +0200

    Installed Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,5 @@ Rails.application.configure do
 
 
   config.action_mailer.default_url_options = { host: 'ninetynine-problems.local' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000'  }
 end
